515f1c2d05633156cf047295a04123d90e81bc53
NTX8205 <ad0124578963@gmail.com>
2021-11-25 11:14:03 +0800

    Update ex_flask_server.py

--- ex_flask_server.py
+++ ex_flask_server.py
@@ -166,5 +166,25 @@ def spider():
         doc_ref.set(doc)
     return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
 
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        info = ""
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]:
+                info += "片名：" + doc.to_dict()["title"] + "<br>"
+                info += "海報：" + doc.to_dict()["picture"] + "<br>"
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>"
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"
+        return info
+    else:
+        return render_template("input.html")
+
+
 if __name__ == "__main__":
     app.run()
